e24af5ff6b5891f7acddf793184172783a5c5ea3
Sudaispakhtoon <sudaiskhankhalil2010pk18@gmail.com>
2025-07-20 15:30:35 +0500

    Add files via upload

---
+++ Sudais.js
@@ -0,0 +1,9 @@
+function ClickMe() {
+    var First = prompt("What's Your Name");
+    alert("Hi " + First + " Welcome to MY Website");
+}
+
+var user =document.getElementById("user");
+
+console.log(user);
+
